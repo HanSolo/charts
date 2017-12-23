25f787dba10050e3ebce7898e95a85c982a57179
Gerrit Grunwald <han.solo@mac.com>
2017-12-23 19:48:42 +0100

    Modifications related to lessons learned in DataViewer

--- src/main/java/eu/hansolo/fx/charts/tools/CtxBounds.java
+++ src/main/java/eu/hansolo/fx/charts/tools/CtxBounds.java
@@ -22,6 +22,10 @@ public class CtxBounds {
     private double y;
     private double width;
     private double height;
+    private double oldX;
+    private double oldY;
+    private double oldWidth;
+    private double oldHeight;
 
 
     // ******************** Constructors **************************************
@@ -40,31 +44,74 @@ public class CtxBounds {
 
 
     // ******************** Methods *******************************************
+    public double getOldX() { return oldX; }
     public double getX() { return x; }
-    public void setX(final double X) { x = X; }
+    public void setX(final double X) {
+        oldX = x;
+        x    = X;
+    }
 
+    public double getOldY() { return oldY; }
     public double getY() { return y; }
-    public void setY(final double Y) { y = Y; }
+    public void setY(final double Y) {
+        oldY = y;
+        y    = Y;
+    }
 
+    public double getOldMinX() { return oldX; }
     public double getMinX() { return x; }
+
+    public double getOldMaxX() { return oldX + oldWidth; }
     public double getMaxX() { return x + width; }
 
+    public double getOldMinY() { return oldY; }
     public double getMinY() { return y; }
+
+    public double getOldMaxY() { return oldY + oldHeight; }
     public double getMaxY() { return y + height; }
 
+    public double getOldWidth() { return oldWidth; }
     public double getWidth() { return width; }
-    public void setWidth(final double WIDTH) { width = Helper.clamp(0, Double.MAX_VALUE, WIDTH); }
+    public void setWidth(final double WIDTH) {
+        oldWidth = width;
+        width    = Helper.clamp(0, Double.MAX_VALUE, WIDTH);
+    }
 
+    public double getOldHeight() { return oldHeight; }
     public double getHeight() { return height; }
-    public void setHeight(final double HEIGHT) { height = Helper.clamp(0, Double.MAX_VALUE, HEIGHT); }
+    public void setHeight(final double HEIGHT) {
+        oldHeight = height;
+        height    = Helper.clamp(0, Double.MAX_VALUE, HEIGHT);
+    }
+
+    public void shiftX(final double SHIFT_X) {
+        setX(x + SHIFT_X);
+    }
+    public void shiftY(final double SHIFT_Y) {
+        setY(x + SHIFT_Y);
+    }
 
     public void set(final CtxBounds BOUNDS) {
-        set(BOUNDS.getX(), BOUNDS.getY(), BOUNDS.getWidth(), BOUNDS.getHeight());
+        setX(BOUNDS.getX());
+        setY(BOUNDS.getY());
+        setWidth(BOUNDS.getWidth());
+        setHeight(BOUNDS.getHeight());
     }
     public void set(final double X, final double Y, final double WIDTH, final double HEIGHT) {
-        x      = X;
-        y      = Y;
-        width  = WIDTH;
-        height = HEIGHT;
+        setX(X);
+        setY(Y);
+        setWidth(WIDTH);
+        setHeight(HEIGHT);
+    }
+
+    @Override public String toString() {
+        return new StringBuilder().append("{\n")
+                                  .append("  \"minX\"  : ").append(getMinX()).append(",\n")
+                                  .append("  \"minY\"  : ").append(getMinY()).append(",\n")
+                                  .append("  \"maxX\"  : ").append(getMaxX()).append(",\n")
+                                  .append("  \"maxY\"  : ").append(getMaxY()).append(",\n")
+                                  .append("  \"width\" : ").append(getWidth()).append(",\n")
+                                  .append("  \"height\": ").append(getHeight()).append("\n")
+                                  .append("}").toString();
     }
 }
