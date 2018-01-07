7fa496682b5ec5d227f3c2abe64f8e1c16db0f59
Gerrit Grunwald <han.solo@mac.com>
2018-01-07 08:10:58 +0100

    Added an AxisBuilder and GridBuilder

---
+++ src/main/java/eu/hansolo/fx/charts/AxisBuilder.java
@@ -0,0 +1,397 @@
+/*
+ * Copyright (c) 2018 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.fx.charts;
+
+import eu.hansolo.fx.charts.tools.TickLabelFormat;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.paint.Color;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+
+/**
+ * User: hansolo
+ * Date: 07.01.18
+ * Time: 06:32
+ */
+public class AxisBuilder<B extends AxisBuilder<B>> {
+    private HashMap<String, Property> properties = new HashMap<>();
+
+
+    // ******************** Constructors **************************************
+    protected AxisBuilder() {}
+
+
+    // ******************** Methods *******************************************
+    public static final AxisBuilder create() {
+        return new AxisBuilder();
+    }
+
+    public final B minValue(final double MIN_VALUE) {
+        properties.put("minValue", new SimpleDoubleProperty(MIN_VALUE));
+        return (B) this;
+    }
+
+    public final B maxValue(final double MAX_VALUE) {
+        properties.put("maxValue", new SimpleDoubleProperty(MAX_VALUE));
+        return (B) this;
+    }
+
+    public final B start(final LocalDateTime DATE_TIME) {
+        properties.put("start", new SimpleObjectProperty<>(DATE_TIME));
+        return (B) this;
+    }
+
+    public final B end(final LocalDateTime DATE_TIME) {
+        properties.put("end", new SimpleObjectProperty<>(DATE_TIME));
+        return (B) this;
+    }
+
+    public final B autoScale(final boolean AUTO) {
+        properties.put("autoScale", new SimpleBooleanProperty(AUTO));
+        return (B) this;
+    }
+
+    public final B title(final String TITLE) {
+        properties.put("title", new SimpleStringProperty(TITLE));
+        return (B) this;
+    }
+
+    public final B unit(final String UNIT) {
+        properties.put("unit", new SimpleStringProperty(UNIT));
+        return (B) this;
+    }
+
+    public final B type(final AxisType TYPE) {
+        properties.put("axisType", new SimpleObjectProperty<>(TYPE));
+        return (B)this;
+    }
+
+    public final B orientation(final Orientation ORIENTATION) {
+        properties.put("orientation", new SimpleObjectProperty(ORIENTATION));
+        return (B)this;
+    }
+
+    public final B position(final Position POS) {
+        properties.put("position", new SimpleObjectProperty(POS));
+        return (B)this;
+    }
+
+    public final B axisBackgroundColor(final Color COLOR) {
+        properties.put("axisBackgroundColor", new SimpleObjectProperty<>(COLOR));
+        return (B)this;
+    }
+
+    public final B axisColor(final Color COLOR) {
+        properties.put("axisColor", new SimpleObjectProperty<>(COLOR));
+        return (B)this;
+    }
+
+    public final B tickLabelColor(final Color COLOR) {
+        properties.put("tickLabelColor", new SimpleObjectProperty<>(COLOR));
+        return (B)this;
+    }
+
+    public final B titleColor(final Color COLOR) {
+        properties.put("titleColor", new SimpleObjectProperty<>(COLOR));
+        return (B)this;
+    }
+
+    public final B minorTickMarkColor(final Color COLOR) {
+        properties.put("minorTickMarkColor", new SimpleObjectProperty<>(COLOR));
+        return (B)this;
+    }
+
+    public final B mediumTickMarkColor(final Color COLOR) {
+        properties.put("mediumTickMarkColor", new SimpleObjectProperty<>(COLOR));
+        return (B)this;
+    }
+
+    public final B majorTickMarkColor(final Color COLOR) {
+        properties.put("majorTickMarkColor", new SimpleObjectProperty<>(COLOR));
+        return (B)this;
+    }
+
+    public final B zeroColor(final Color COLOR) {
+        properties.put("zeroColor", new SimpleObjectProperty<>(COLOR));
+        return (B)this;
+    }
+
+    public final B minorTickSpace(final double SPACE) {
+        properties.put("minorTickSpace", new SimpleDoubleProperty(SPACE));
+        return (B)this;
+    }
+
+    public final B majorTickSpace(final double SPACE) {
+        properties.put("majorTickSpace", new SimpleDoubleProperty(SPACE));
+        return (B)this;
+    }
+
+    public final B tickLabelsVisible(final boolean VISIBLE) {
+        properties.put("tickLabelsVisible", new SimpleBooleanProperty(VISIBLE));
+        return (B)this;
+    }
+
+    public final B onlyFirstAndLastTickLabelVisible(final boolean VISIBLE) {
+        properties.put("onlyFirstAndLastTickLabelVisible", new SimpleBooleanProperty(VISIBLE));
+        return (B)this;
+    }
+
+    public final B locale(final Locale LOCALE) {
+        properties.put("locale", new SimpleObjectProperty<>(LOCALE));
+        return (B)this;
+    }
+
+    public final B decimals(final int DECIMALS) {
+        properties.put("decimals", new SimpleIntegerProperty(DECIMALS));
+        return (B)this;
+    }
+
+    public final B tickLabelOrientation(final TickLabelOrientation ORIENTATION) {
+        properties.put("tickLabelOrientation", new SimpleObjectProperty<>(ORIENTATION));
+        return (B)this;
+    }
+
+    public final B tickLabelFormat(final TickLabelFormat FORMAT) {
+        properties.put("tickLabelFormat", new SimpleObjectProperty<>(FORMAT));
+        return (B)this;
+    }
+
+    public final B autoFontSize(final boolean AUTO) {
+        properties.put("autoFontSize", new SimpleBooleanProperty(AUTO));
+        return (B)this;
+    }
+
+    public final B tickLabelFontSize(final double SIZE) {
+        properties.put("tickLabelFontSize", new SimpleDoubleProperty(SIZE));
+        return (B)this;
+    }
+
+    public final B titleFontSize(final double SIZE) {
+        properties.put("titleFontSize", new SimpleDoubleProperty(SIZE));
+        return (B)this;
+    }
+
+    public final B zoneId(final ZoneId  ID) {
+        properties.put("zoneId", new SimpleObjectProperty<>(ID));
+        return (B)this;
+    }
+
+    public final B dateTimeFormatPattern(final String PATTERN) {
+        properties.put("dateTimeFormatPattern", new SimpleStringProperty(PATTERN));
+        return (B)this;
+    }
+
+
+    // General properties
+    public final B prefSize(final double WIDTH, final double HEIGHT) {
+        properties.put("prefSize", new SimpleObjectProperty<>(new Dimension2D(WIDTH, HEIGHT)));
+        return (B) this;
+    }
+    public final B minSize(final double WIDTH, final double HEIGHT) {
+        properties.put("minSize", new SimpleObjectProperty<>(new Dimension2D(WIDTH, HEIGHT)));
+        return (B) this;
+    }
+    public final B maxSize(final double WIDTH, final double HEIGHT) {
+        properties.put("maxSize", new SimpleObjectProperty<>(new Dimension2D(WIDTH, HEIGHT)));
+        return (B) this;
+    }
+
+    public final B prefWidth(final double PREF_WIDTH) {
+        properties.put("prefWidth", new SimpleDoubleProperty(PREF_WIDTH));
+        return (B) this;
+    }
+    public final B prefHeight(final double PREF_HEIGHT) {
+        properties.put("prefHeight", new SimpleDoubleProperty(PREF_HEIGHT));
+        return (B) this;
+    }
+
+    public final B minWidth(final double MIN_WIDTH) {
+        properties.put("minWidth", new SimpleDoubleProperty(MIN_WIDTH));
+        return (B) this;
+    }
+    public final B minHeight(final double MIN_HEIGHT) {
+        properties.put("minHeight", new SimpleDoubleProperty(MIN_HEIGHT));
+        return (B) this;
+    }
+
+    public final B maxWidth(final double MAX_WIDTH) {
+        properties.put("maxWidth", new SimpleDoubleProperty(MAX_WIDTH));
+        return (B) this;
+    }
+    public final B maxHeight(final double MAX_HEIGHT) {
+        properties.put("maxHeight", new SimpleDoubleProperty(MAX_HEIGHT));
+        return (B) this;
+    }
+
+    public final B scaleX(final double SCALE_X) {
+        properties.put("scaleX", new SimpleDoubleProperty(SCALE_X));
+        return (B) this;
+    }
+    public final B scaleY(final double SCALE_Y) {
+        properties.put("scaleY", new SimpleDoubleProperty(SCALE_Y));
+        return (B) this;
+    }
+
+    public final B layoutX(final double LAYOUT_X) {
+        properties.put("layoutX", new SimpleDoubleProperty(LAYOUT_X));
+        return (B) this;
+    }
+    public final B layoutY(final double LAYOUT_Y) {
+        properties.put("layoutY", new SimpleDoubleProperty(LAYOUT_Y));
+        return (B) this;
+    }
+
+    public final B translateX(final double TRANSLATE_X) {
+        properties.put("translateX", new SimpleDoubleProperty(TRANSLATE_X));
+        return (B) this;
+    }
+    public final B translateY(final double TRANSLATE_Y) {
+        properties.put("translateY", new SimpleDoubleProperty(TRANSLATE_Y));
+        return (B) this;
+    }
+
+    public final B padding(final Insets INSETS) {
+        properties.put("padding", new SimpleObjectProperty<>(INSETS));
+        return (B) this;
+    }
+
+
+    public final Axis build() {
+        final Axis CONTROL = new Axis();
+
+        for (String key : properties.keySet()) {
+            if ("prefSize".equals(key)) {
+                Dimension2D dim = ((ObjectProperty<Dimension2D>) properties.get(key)).get();
+                CONTROL.setPrefSize(dim.getWidth(), dim.getHeight());
+            } else if ("minSize".equals(key)) {
+                Dimension2D dim = ((ObjectProperty<Dimension2D>) properties.get(key)).get();
+                CONTROL.setMinSize(dim.getWidth(), dim.getHeight());
+            } else if ("maxSize".equals(key)) {
+                Dimension2D dim = ((ObjectProperty<Dimension2D>) properties.get(key)).get();
+                CONTROL.setMaxSize(dim.getWidth(), dim.getHeight());
+            } else if ("prefWidth".equals(key)) {
+                CONTROL.setPrefWidth(((DoubleProperty) properties.get(key)).get());
+            } else if ("prefHeight".equals(key)) {
+                CONTROL.setPrefHeight(((DoubleProperty) properties.get(key)).get());
+            } else if ("minWidth".equals(key)) {
+                CONTROL.setMinWidth(((DoubleProperty) properties.get(key)).get());
+            } else if ("minHeight".equals(key)) {
+                CONTROL.setMinHeight(((DoubleProperty) properties.get(key)).get());
+            } else if ("maxWidth".equals(key)) {
+                CONTROL.setMaxWidth(((DoubleProperty) properties.get(key)).get());
+            } else if ("maxHeight".equals(key)) {
+                CONTROL.setMaxHeight(((DoubleProperty) properties.get(key)).get());
+            } else if ("scaleX".equals(key)) {
+                CONTROL.setScaleX(((DoubleProperty) properties.get(key)).get());
+            } else if ("scaleY".equals(key)) {
+                CONTROL.setScaleY(((DoubleProperty) properties.get(key)).get());
+            } else if ("layoutX".equals(key)) {
+                CONTROL.setLayoutX(((DoubleProperty) properties.get(key)).get());
+            } else if ("layoutY".equals(key)) {
+                CONTROL.setLayoutY(((DoubleProperty) properties.get(key)).get());
+            } else if ("translateX".equals(key)) {
+                CONTROL.setTranslateX(((DoubleProperty) properties.get(key)).get());
+            } else if ("translateY".equals(key)) {
+                CONTROL.setTranslateY(((DoubleProperty) properties.get(key)).get());
+            } else if ("padding".equals(key)) {
+                CONTROL.setPadding(((ObjectProperty<Insets>) properties.get(key)).get());
+            } // Control specific properties
+              else if ("minValue".equals(key)) {
+                CONTROL.setMinValue(((DoubleProperty) properties.get(key)).get());
+            } else if ("maxValue".equals(key)) {
+                CONTROL.setMaxValue(((DoubleProperty) properties.get(key)).get());
+            } else if ("start".equals(key)) {
+                CONTROL.setStart(((ObjectProperty<LocalDateTime>) properties.get(key)).get());
+            } else if ("end".equals(key)) {
+                CONTROL.setEnd(((ObjectProperty<LocalDateTime>) properties.get(key)).get());
+            } else if ("autoScale".equals(key)) {
+                CONTROL.setAutoScale(((BooleanProperty) properties.get(key)).get());
+            } else if ("title".equals(key)) {
+                CONTROL.setTitle(((StringProperty) properties.get(key)).get());
+            } else if ("unit".equals(key)) {
+                CONTROL.setUnit(((StringProperty) properties.get(key)).get());
+            } else if ("axisType".equals(key)) {
+                CONTROL.setType(((ObjectProperty<AxisType>) properties.get(key)).get());
+            } else if ("orientation".equals(key)) {
+                CONTROL.setOrientation(((ObjectProperty<Orientation>) properties.get(key)).get());
+            } else if ("position".equals(key)) {
+                CONTROL.setPosition(((ObjectProperty<Position>) properties.get(key)).get());
+            } else if ("axisBackgroundColor".equals(key)) {
+                CONTROL.setAxisBackgroundColor(((ObjectProperty<Color>) properties.get(key)).get());
+            } else if ("axisColor".equals(key)) {
+                CONTROL.setAxisColor(((ObjectProperty<Color>) properties.get(key)).get());
+            } else if ("tickLabelColor".equals(key)) {
+                CONTROL.setTickLabelColor(((ObjectProperty<Color>) properties.get(key)).get());
+            } else if ("titleColor".equals(key)) {
+                CONTROL.setTitleColor(((ObjectProperty<Color>) properties.get(key)).get());
+            } else if ("minorTickMarkColor".equals(key)) {
+                CONTROL.setMinorTickMarkColor(((ObjectProperty<Color>) properties.get(key)).get());
+            } else if ("mediumTickMarkColor".equals(key)) {
+                CONTROL.setMediumTickMarkColor(((ObjectProperty<Color>) properties.get(key)).get());
+            } else if ("majorTickMarkColor".equals(key)) {
+                CONTROL.setMajorTickMarkColor(((ObjectProperty<Color>) properties.get(key)).get());
+            } else if ("zeroColor".equals(key)) {
+                CONTROL.setZeroColor(((ObjectProperty<Color>) properties.get(key)).get());
+            } else if ("minorTickSpace".equals(key)) {
+                CONTROL.setMinorTickSpace(((DoubleProperty) properties.get(key)).get());
+            } else if ("majorTickSpace".equals(key)) {
+                CONTROL.setMajorTickSpace(((DoubleProperty) properties.get(key)).get());
+            } else if ("tickLabelsVisible".equals(key)) {
+                CONTROL.setTickLabelsVisible(((BooleanProperty) properties.get(key)).get());
+            } else if ("onlyFirstAndLastTickLabel".equals(key)) {
+                CONTROL.setOnlyFirstAndLastTickLabelVisible(((BooleanProperty) properties.get(key)).get());
+            } else if ("local".equals(key)) {
+                CONTROL.setLocale(((ObjectProperty<Locale>) properties.get(key)).get());
+            } else if ("decimals".equals(key)) {
+                CONTROL.setDecimals(((IntegerProperty) properties.get(key)).get());
+            } else if ("tickLabelOrientation".equals(key)) {
+                CONTROL.setTickLabelOrientation(((ObjectProperty<TickLabelOrientation>) properties.get(key)).get());
+            } else if ("tickLabelFormat".equals(key)) {
+                CONTROL.setTickLabelFormat(((ObjectProperty<TickLabelFormat>) properties.get(key)).get());
+            } else if ("autoFontSize".equals(key)) {
+                CONTROL.setAutoFontSize(((BooleanProperty) properties.get(key)).get());
+            } else if ("tickLabelFontSize".equals(key)) {
+                CONTROL.setTickLabelFontSize(((DoubleProperty) properties.get(key)).get());
+            } else if ("titleFontSize".equals(key)) {
+                CONTROL.setTitleFontSize(((DoubleProperty) properties.get(key)).get());
+            } else if ("zoneId".equals(key)) {
+                CONTROL.setZoneId(((ObjectProperty<ZoneId>) properties.get(key)).get());
+            } else if ("dateTimeFormatPattern".equals(key)) {
+                CONTROL.setDateTimeFormatPattern(((StringProperty) properties.get(key)).get());
+            }
+        }
+        return CONTROL;
+    }
+}
