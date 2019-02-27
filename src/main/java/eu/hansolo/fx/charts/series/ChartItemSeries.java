c9f0a0ca2915a40a941b147cd9a375443d846a28
Gerrit Grunwald <han.solo@mac.com>
2019-02-27 12:28:36 +0100

    import statement clean up

--- src/main/java/eu/hansolo/fx/charts/series/ChartItemSeries.java
+++ src/main/java/eu/hansolo/fx/charts/series/ChartItemSeries.java
@@ -20,9 +20,7 @@ import eu.hansolo.fx.charts.ChartType;
 import eu.hansolo.fx.charts.Symbol;
 import eu.hansolo.fx.charts.data.ChartItem;
 import javafx.collections.ObservableList;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import sun.security.timestamp.TSRequest;
 
 import java.util.Arrays;
 import java.util.List;
