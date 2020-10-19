cb9133e9e66bd66292eff16cd3ea17a49152473a
Gerrit Grunwald <han.solo@mac.com>
2020-10-19 14:15:26 +0200

    Added missing exports (issue #27)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,5 +11,19 @@ module charts {
     requires javafx.swing;
 
     exports eu.hansolo.fx.geometry;
+    exports eu.hansolo.fx.geometry.tools;
+    exports eu.hansolo.fx.geometry.transform;
     exports eu.hansolo.fx.charts;
+    exports eu.hansolo.fx.charts.areaheatmap;
+    exports eu.hansolo.fx.charts.color;
+    exports eu.hansolo.fx.charts.converter;
+    exports eu.hansolo.fx.charts.data;
+    exports eu.hansolo.fx.charts.event;
+    exports eu.hansolo.fx.charts.font;
+    exports eu.hansolo.fx.charts.forcedirectedgraph;
+    exports eu.hansolo.fx.charts.heatmap;
+    exports eu.hansolo.fx.charts.pareto;
+    exports eu.hansolo.fx.charts.series;
+    exports eu.hansolo.fx.charts.tools;
+    exports eu.hansolo.fx.charts.world;
 }
