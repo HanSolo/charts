74d263980af3515f8706fee58b45bf900b7660a1
Gerrit Grunwald <han.solo@mac.com>
2023-12-05 18:17:14 +0100

    Simplified the OfflineRenderTest2 chart

--- src/test/java/eu/hansolo/fx/charts/OfflineRenderTest2.java
+++ src/test/java/eu/hansolo/fx/charts/OfflineRenderTest2.java
@@ -57,135 +57,29 @@ public class OfflineRenderTest2 {
         Helper.initFXPlatform();
         init();
 
-        //testRenderToFile();
-        testRenderToBufferedImage();
+        testRenderToFile();
+        //testRenderToBufferedImage();
     }
 
     private void init() {
-        // Setup chart items
-        PlotItem brazil = new PlotItem("Brazil", Colors.LIGHT_BLUE.get(), 0);
-        PlotItem mexico = new PlotItem("Mexico", Colors.ORANGE.get(), 0);
-        PlotItem usa    = new PlotItem("USA", Colors.ORANGE.get(), 0);
-        PlotItem canada = new PlotItem("Canada", Colors.LIGHT_RED.get(), 0);
+        PlotItem coal        = new PlotItem("Coal", Colors.GREEN.get(), 0);
+        PlotItem naturalGas  = new PlotItem("Natural Gas", Colors.ORANGE.get().darker(), 0);
+        PlotItem oil         = new PlotItem("Oil", Colors.ORANGE.get(), 0);
+        PlotItem fossilFuels = new PlotItem("Fossil Fuels", Colors.BLUE.get(), 1);
+        PlotItem electricity = new PlotItem("Electricity", Colors.LIGHT_BLUE.get(), 1);
+        PlotItem energy      = new PlotItem("Energy", Color.GRAY, 2);
 
-        PlotItem germany     = new PlotItem("Germany", Color.web("#FF48C6"), 1);
-
-        PlotItem portugal    = new PlotItem("Portugal", Colors.LIGHT_BLUE.get(), 1);
-        PlotItem spain       = new PlotItem("Spain", Colors.LIGHT_GREEN.get(), 1);
-        PlotItem england     = new PlotItem("England", Colors.LIGHT_RED.get(), 1);
-        PlotItem france      = new PlotItem("France", Colors.LIGHT_GREEN.get(), 1);
-
-        PlotItem southAfrica = new PlotItem("South Africa", Colors.YELLOW.get(), 2);
-        PlotItem angola      = new PlotItem("Angola", Colors.PURPLE.get(), 2);
-        PlotItem morocco     = new PlotItem("Morocco", Colors.YELLOW.get(), 2);
-        PlotItem senegal     = new PlotItem("Senegal", Colors.PURPLE.get(), 2);
-        PlotItem mali        = new PlotItem("Mali", Colors.BLUE.get(), 2);
-
-        PlotItem china       = new PlotItem("China", Colors.BLUE.get(), 3);
-        PlotItem japan       = new PlotItem("Japan", Colors.GREEN.get(), 3);
-        PlotItem india       = new PlotItem("India", Colors.GREEN.get(), 3);
-
-        PlotItem australia   = new PlotItem("Australia", Colors.ORANGE.get(), 4);
-
-        // Setup vertical text positions
-        //australia.setVerticalTextPosition(Position.TOP);
-        germany.setVerticalTextPosition(Position.BOTTOM);
-
-        // Setup item text color
-        australia.setTextColor(Color.RED);
-        germany.setTextColor(Color.MAGENTA);
-
-        // Setup flows
-        brazil.addToOutgoing(portugal, 5);
-        brazil.addToOutgoing(france, 1);
-        brazil.addToOutgoing(spain, 1);
-        brazil.addToOutgoing(england, 1);
-        canada.addToOutgoing(portugal, 1);
-        canada.addToOutgoing(france, 5);
-        canada.addToOutgoing(england, 1);
-        mexico.addToOutgoing(portugal, 1);
-        mexico.addToOutgoing(france, 1);
-        mexico.addToOutgoing(spain, 5);
-        mexico.addToOutgoing(england, 1);
-        usa.addToOutgoing(portugal, 1);
-        usa.addToOutgoing(france, 1);
-        usa.addToOutgoing(spain, 1);
-        usa.addToOutgoing(england, 5);
-
-        germany.addToOutgoing(southAfrica, 5);
-
-        portugal.addToOutgoing(angola, 2);
-        portugal.addToOutgoing(senegal, 1);
-        portugal.addToOutgoing(morocco, 1);
-        portugal.addToOutgoing(southAfrica, 3);
-        france.addToOutgoing(angola, 1);
-        france.addToOutgoing(senegal, 3);
-        france.addToOutgoing(mali, 3);
-        france.addToOutgoing(morocco, 3);
-        france.addToOutgoing(southAfrica, 1);
-        spain.addToOutgoing(senegal, 1);
-        spain.addToOutgoing(morocco, 3);
-        spain.addToOutgoing(southAfrica, 1);
-        england.addToOutgoing(angola, 1);
-        england.addToOutgoing(senegal, 1);
-        england.addToOutgoing(morocco, 2);
-        england.addToOutgoing(southAfrica, 7);
-        southAfrica.addToOutgoing(china, 5);
-        southAfrica.addToOutgoing(india, 1);
-        southAfrica.addToOutgoing(japan, 3);
-        angola.addToOutgoing(china, 5);
-        angola.addToOutgoing(india, 1);
-        angola.addToOutgoing(japan, 3);
-        senegal.addToOutgoing(china, 5);
-        senegal.addToOutgoing(india, 1);
-        senegal.addToOutgoing(japan, 3);
-        mali.addToOutgoing(china, 5);
-        mali.addToOutgoing(india, 1);
-        mali.addToOutgoing(japan, 3);
-        morocco.addToOutgoing(china, 5);
-        morocco.addToOutgoing(india, 1);
-        morocco.addToOutgoing(japan, 3);
-
-        china.addToOutgoing(australia, 3);
-        //japan.addToOutgoing(australia, 1);
-        india.addToOutgoing(australia, 2);
+        coal.addToOutgoing(fossilFuels, 25);
+        coal.addToOutgoing(electricity, 25);
+        naturalGas.addToOutgoing(fossilFuels, 20);
+        oil.addToOutgoing(fossilFuels, 15);
+        fossilFuels.addToOutgoing(energy, 60);
+        electricity.addToOutgoing(energy, 25);
 
         sankeyPlot = SankeyPlotBuilder.create()
-                                      .prefSize(600, 400)
-                                      .items(brazil, mexico, usa, canada,
-                                             germany,
-                                             portugal, spain, england, france,
-                                             southAfrica, angola, morocco, senegal, mali,
-                                             china, japan, india,
-                                             australia)
-                                      //.useItemColor(false)
-                                      //.itemColor(Color.RED)
+                                      .items(coal, naturalGas, oil, fossilFuels, electricity, energy)
                                       .streamFillMode(StreamFillMode.GRADIENT)
-                                      .selectionColor(Color.rgb(0, 100, 240, 0.5))
-                                      //.streamColor(Color.rgb(200, 0, 0, 0.25))
-                                      //.textColor(Color.RED)
-                                      //.autoItemWidth(false)
-                                      //.itemWidth(5)
-                                      //.autoItemGap(false)
-                                      //.itemGap(10)
-                                      //.showFlowDirection(true)
-                                      .useCustomFont(true)
-                                      .useItemTextColor(true)
-                                      .autoAdjustVerticalTextPosition(true)
                                       .build();
-        sankeyPlot.getItems().forEach(item -> item.addChartEvtObserver(ChartEvt.ANY, e -> {
-            if (ChartEvt.ITEM_SELECTED.equals(e.getEvtType())) {
-                PlotItem sourceItem = (PlotItem) e.getSource();
-                PlotItem targetItem = (PlotItem) e.getTarget();
-                if (null != sourceItem && null != targetItem) {
-                    double value = sourceItem.getOutgoing().get(targetItem);
-                    System.out.println(sourceItem.getName() + " -> " + targetItem.getName() + " => connection value: " + value);
-                } else if (null != sourceItem) {
-                    double value = sourceItem.getSumOfOutgoing();
-                    System.out.println(sourceItem.getName() + " => sum of outgoing values: " + value);
-                }
-            }
-        }));
     }
 
     private void testRenderToFile() {
